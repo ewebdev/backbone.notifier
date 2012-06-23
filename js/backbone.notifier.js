a02936c5a2cf4f9b591d2628fbc31099e0ca42a8
Eyal Weiss <weiss.e@gmail.com>
2012-06-23 05:26:23 +0300

    modules architecture 3D module improvements to examples page (syntax highlight, css, content)

--- js/backbone.notifier.js
+++ js/backbone.notifier.js
@@ -42,16 +42,16 @@
 			},
 			transitions: {
 				top: {
-					'in': function(el, inner, options, duration){
-						el.animate({top: options.top, opacity: options.opacity}, duration);
+					'in': function(el, inner, options, duration, callback){
+						el.animate({top: options.top, opacity: options.opacity}, duration, callback || emptyFn);
 					},
 					'out': function(el, inner, options, duration, callback){
 						el.animate({top: -inner.height(), opacity: 0}, duration, callback || emptyFn);
 					}
 				},
 				center: {
-					'in': function(el, inner, options, duration){
-						el.animate({ top: '50%', marginTop: -inner.height()/2, opacity: options.opacity}, duration);
+					'in': function(el, inner, options, duration, callback){
+						el.animate({ top: '50%', marginTop: -inner.height()/2, opacity: options.opacity}, duration, callback || emptyFn);
 					},
 					'out': function(el, inner, options, duration, callback){
 						el.animate({top: '0%', marginTop: -inner.height(), opacity: 0}, duration, callback || emptyFn);
@@ -129,6 +129,14 @@
 				}
 				return i;
 		    },
+			getWrapperCls: function(settings){
+		   		return [settings.baseCls,
+					   settings.cls ? settings.baseCls + '-' + settings.cls : '',
+					   settings.baseCls + '-pos-' + settings.position,
+					   settings.buttons ? settings.baseCls + '-block' : '',
+					   settings.loader ? settings.baseCls + '-with-loader' : ''
+				   ].join(' ');
+			},
 		    notify: function(options){
 				if (_.isString(options)){
 					options = {message: options};
@@ -150,18 +158,14 @@
 		    	}
 				var zIndex = scope.calcZIndex.call(scope);
 
-				settings.wrapperCls = [settings.baseCls,
-							settings.cls ? settings.baseCls + '-' + settings.cls : '',
-							settings.baseCls + '-pos-' + settings.position,
-							settings.buttons ? settings.baseCls + '-block' : '',
-							settings.loader ? settings.baseCls + '-with-loader' : ''
-						].join(' ');
+				settings.wrapperCls = this.getWrapperCls(settings);
 
 				settings.innerCls = settings.baseCls + '-inner';
 
 				var msgEl = $(settings.template(settings)),
 					msgInner = msgEl.find('.' + settings.innerCls);
 
+				this.eat('beforeAppendMsgEl', settings, msgEl, msgInner);
 				msgEl.css({top: settings.top - 40, opacity: 0, zIndex: settings.modal ? ++zIndex : zIndex}).prependTo(this.$el);
 				var msgView = new this.NotificationView({
 		    		el: msgEl
@@ -169,7 +173,6 @@
 		    	msgView.settings = settings;
 
 				if (settings.buttons || msgInner.find('button').length) {
-//					msgEl.addClass(settings.baseCls + '-block');
 					msgInner.on('button click', function(e){
 						msgView.trigger('click:' + $(e.target).data('role'));
 					});
@@ -181,15 +184,17 @@
 	    				e.stopPropagation();
 	    			} 
 	    			msgView.trigger('beforeHide', msgView, msgEl);
-	    			settings.modal && msgView.screenEl.fadeOut(300, function(){
+					settings.modal && msgView.screenEl.fadeOut(300, function(){
 						msgView.trigger('screenHidden', msgView, msgEl);
 						msgView.screenEl.remove();
 	    			});
 
+					scope.eat.call(scope, 'beforeHideMsgEl', settings, msgEl, msgInner, msgView);
 					var outAnimFn = scope.transitions[settings.position].out;
 					outAnimFn.call(scope, msgEl, msgInner, settings, options.fadeOutMs, function(){
 						msgView.remove();
 						msgView.trigger('destroyed', msgView, msgEl);
+						scope.eat.call(scope, 'afterDestroyMsgEl', settings, msgEl, msgInner, msgView);
 						_.isFunction(e) && e.call(msgView, msgView, msgEl);
 					});
 					if (msgView.timeoutId) {
@@ -225,10 +230,78 @@
 				var animateFn = this.transitions[settings.position]['in'];
 				scope.current[msgView.cid] = msgView;
 				msgView.zIndex = zIndex;
-				animateFn.call(this, msgEl, msgInner, settings, settings.fadeInMs);
 				settings.css && msgInner.css(settings.css);
+
+				this.eat.call(this, 'beforeAnimateInMsgEl', settings, msgEl, msgInner, msgView);
+				animateFn.call(this, msgEl, msgInner, settings, settings.fadeInMs, function(){
+					scope.eat.call(scope, 'afterAnimateInMsgEl', settings, msgEl, msgInner, msgView);
+				});
 		    	return msgView;
-		    }
+		    },
+
+			eat: function(event, settings, msgEl, msgInner, msgView){ // Triggers registered modules' events
+				var obj = Notifier.prototype['@' + event];
+				obj && _.each(obj, function(e, moduleName){
+					e.fn.call(e.caller, this, settings, msgEl, msgInner, msgView);
+				}, this);
+			}
+//			,
+//			'@beforeAppendMsgEl': {},
+//			'@beforeAnimateInMsgEl': {},
+//			'@afterAnimateInMsgEl': {},
+//			'@beforeHideMsgEl': {},
+//			'@afterDestroyMsgEl': {}
 	});
+
+
+	// ====================== Modules mechanism ======================
+	Notifier.getModule = function(moduleName){
+		return (_.isObject(moduleName)) ? moduleName : Notifier.modules[moduleName];
+	};
+
+	Notifier.regModule = function(moduleName, m){
+		if (arguments.length === 1) {
+			m = $.isFunction(moduleName) ? moduleName() : moduleName;
+			moduleName = m.name;
+		} else {
+			m = $.isFunction(m) ? m() : m;
+			m.name = moduleName;
+		}
+		if (m.extend) {
+			Notifier.prototype = $.extend(true, {}, Notifier.prototype, m.extend );
+		}
+		(Notifier.modules = Notifier.modules || {})[moduleName] = m;
+		$.isFunction(m.register) && m.register.call(m, Notifier);
+		m.enabled && Notifier.enableModule(m);
+	};
+
+	Notifier.enableModule = function(moduleName){
+		var m = Notifier.getModule(moduleName);
+		if (m) {
+			$.each(m.events, function(k, fn){
+				var arr = Notifier.prototype['@' + k] = Notifier.prototype['@' + k] || {};
+				arr[m.name] = {caller: m, fn: fn};
+			});
+			m.enabled = true;
+			$.isFunction(m.enable) && m.enable.call(m, Notifier);
+			return m;
+		}
+		console.warn('module "'  + moduleName + '" is not registered.');
+		return false;
+	};
+
+	Notifier.disableModule = function(moduleName){
+		var m = Notifier.getModule(moduleName);
+		if (m) {
+			$.each(Notifier.modules[m.name].events, function(k, fn){
+				delete Notifier.prototype['@' + k];
+			});
+			m.enabled = false;
+			$.isFunction(m.disable) && m.disable.call(m, Notifier);
+			return m;
+		}
+		console.warn('module "'  + moduleName + '" is not registered.');
+		return false;
+	};
 
 })(jQuery, Backbone, _);
